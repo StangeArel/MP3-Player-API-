82a41928c4627c9c5cef7e16c9b97fef46bb6408
Valeriya Stange <valeriya.stange@gmail.com>
2025-06-14 13:50:36 +0200

    setPlayerDisplay the song

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
     playButton.classList.add("playing");
 
     highlightCurrentSong();
+    setPlayerDisplay();
     audio.play();
 };
 
@@ -128,6 +129,16 @@ const playPreviousSong = () => {
 
         playSong(previousSong.id);
     }
+};
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
@@ -163,6 +174,11 @@ const renderSongs = (array) => {
         .join("");
 
     playlistSongs.innerHTML = songsHTML;
+};
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
